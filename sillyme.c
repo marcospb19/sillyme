bb0ace6ff36f528631ddc91dc86c129e3fe11fba
João Marcos <marcospb19@hotmail.com>
2019-06-04 01:46:14 -0300

    Added sillyme.c with main function

---
+++ sillyme.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+//          _ ____
+//     ___ (_) / /_ ____ _  ___
+//    (_-</ / / / // /  ' \/ -_)
+//   /___/_/_/_/\_, /_/_/_/\__/
+//             /___/
+
+// From https://github.com/marcospb19/sillyme
+
+
+int main()
+{
+	printf("Silly Me!\n");
+}
